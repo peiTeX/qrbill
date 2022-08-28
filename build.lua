074712f47e248d29e7c1487018ee61d1fbc8e20a
Marei (peiTeX) <marei@peitex.de>
2022-08-28 09:19:03 +0200

    add qrbill-*.lua to install

--- build.lua
+++ build.lua
@@ -10,9 +10,11 @@ packtdszip = true
 
 installfiles = {"*.sty","qrbill_*.pdf","*.qrbill-cfg.tex", "*.csv", "*-demo.tex"}
 
+scriptfiles = {"qrbill-*.lua"}
+
 demofiles = {"*-demo.tex"}
 docfiles = {"*-demo.tex"}
-sourcefiles = {"*.dtx", "*.ins", "*.csv"}
+sourcefiles = {"*.dtx", "*.ins", "*.csv", "qrbill-*.lua"}
 
 tdslocations = {
 	     "tex/latex/qrbill/qrbill_*.pdf",
