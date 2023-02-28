19a793c7e82d86166dfe68656c6979b4bcfa622d
Marei (peiTeX) <marei@peitex.de>
2023-02-28 15:15:50 +0100

    prefix qrencode.lua as CTAN asked for

--- build.lua
+++ build.lua
@@ -2,19 +2,22 @@
 
 module = "qrbill"
 
+cp("","libraries/luaqrcode/qrencode.lua","qrbill-qrencode.lua")
+
 typesetfiles  = {"*.dtx"}
 typesetexe = "lualatex"
 typesetopts = "--shell-escape"
 
 packtdszip = true
 
-installfiles = {"*.sty","qrbill_*.pdf","*.qrbill-cfg.tex", "*.csv", "*-demo.tex","libraries/luaqrcode/qrencode.lua","qrbill-*.lua"}
+installfiles = {"*.sty","qrbill_*.pdf","*.qrbill-cfg.tex", "*.csv", "*-demo.tex","qrbill-*.lua"}
 
-scriptfiles = {"qrbill-*.lua","qrencode.lua"}
+scriptfiles = {"qrbill-*.lua"}
 
 demofiles = {"*-demo.tex"}
 docfiles = {"*-demo.tex"}
-sourcefiles = {"*.dtx", "*.ins", "*.csv", "qrbill-*.lua", "libraries/luaqrcode/qrencode.lua"}
+sourcefiles = {"*.dtx", "*.ins", "*.csv", "qrbill-*.lua"}
+cleanfiles={"*.log", "*.pdf", "*.zip", "qrbill-qrencode.lua"}
 
 tdslocations = {
 	     "tex/latex/qrbill/qrbill_*.pdf",
